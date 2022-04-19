fdede4eeeb52d5929eb33e3c9213e0a91ecb07ab
A4kash <aakashsinha@utexas.edu>
2022-04-19 17:54:24 -0500

    Added Animation to 1st image!

--- app/src/main/java/com/example/coffeeshopprototype/MainActivity.kt
+++ app/src/main/java/com/example/coffeeshopprototype/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.coffeeshopprototype
 
+import android.animation.ObjectAnimator
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.ImageView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -14,6 +17,7 @@ import com.example.coffeeshopprototype.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,5 +47,13 @@ class MainActivity : AppCompatActivity() {
     fun launchGPS(view: View){
         val intent = Intent(this, MapsActivity::class.java)
         startActivity(intent)
+    }
+
+    fun rotater(view: View){
+        val ImageView = view.findViewById<ImageView>(R.id.black_coffee)
+        ImageView.animate().apply{
+            duration = 1000
+            rotationBy(360f)
+        }.start()
     }
 }
